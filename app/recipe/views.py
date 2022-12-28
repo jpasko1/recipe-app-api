130a79823cffb7e6f0b221060e53dc3e812e27bf
Joe Pasko <joe.pasko@alchemer.com>
2022-12-28 15:19:46 -0700

    Correct id to be -id

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -13,8 +13,8 @@ class RecipeViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.RecipeSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
-    permssion_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         """Retrive recipes for authenticated user."""
-        return self.queryset.filter(user=self.request.user).order_by('id')
+        return self.queryset.filter(user=self.request.user).order_by('-id')
