abae7958d019fe8235830433aa7dbaa322857696
Joe Pasko <joe.pasko@alchemer.com>
2022-12-28 15:13:51 -0700

    add recipe views

---
+++ app/recipe/urls.py
@@ -0,0 +1,23 @@
+"""
+URL mappingss for the recipe app.
+"""
+
+from django.urls import (
+    path,
+    include,
+)
+
+from rest_framework.routers import DefaultRouter
+
+from recipe import views
+
+router = DefaultRouter()
+router.register('recipes', views.RecipeViewSet)
+
+app_name = 'recipe' # name used when doing reverse URL lookups
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
+
