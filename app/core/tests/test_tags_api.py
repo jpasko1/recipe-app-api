4d781c8479b3f25394e2a0ed0b63740a5c6f1148
Joe Pasko <joe.pasko@alchemer.com>
2023-03-13 13:05:57 -0600

    Fix typo

--- app/core/tests/test_tags_api.py
+++ app/core/tests/test_tags_api.py
@@ -45,7 +45,7 @@ class PrivateTagsApiTests(TestCase):
 
         tags = Tag.objects.all().order_by('-name')
 
-        serializer = TagSerializer(tags, name=True)
+        serializer = TagSerializer(tags, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
 
